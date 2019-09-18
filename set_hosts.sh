4d24c13740f08470b8071a4cf64b79e0a0684727
Your Name <you@example.com>
2019-09-19 00:32:14 +0200

    Initial import.

--- set_hosts.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-curl localhost:1500 >.tmp.json 2>/dev/null
-
-ipm=`cat .tmp.json | python2.7 -c 'import sys; import json; j=json.loads(sys.stdin.read()); print([h["main_ip"] for h in [j[k] for k in j.keys()] if h["OSID"] == "270"][0])'`
-ipw1=`curl .tmp.json | python2.7 -c 'import sys; import json; j=json.loads(sys.stdin.read()); print([h["main_ip"] for h in [j[k] for k in j.keys()] if h["OSID"] == "270"][1])'`
-
-perl -pe "s/MASTER/${ipm}/;s/WORKER/${ipw1}/" < hosts.templ > hosts
